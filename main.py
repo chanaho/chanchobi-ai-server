f52cb738425f24b73a4f9a8cb62f2465fa953534
ChanCho <chancho@example.com>
2026-07-10 06:58:07 +0900

    add onnx output debug

--- main.py
+++ main.py
@@ -256,7 +256,27 @@ async def predict(
 
         print(
             "BEST SCORE :",
-            round(best_score, 4)
+            round(best_score, 4)            
+        )
+
+        print(
+            "BEST INDEX :",
+            best_idx
+        )
+
+        print(
+            "BEST ROW :",
+            np.round(best, 4)
+        )
+
+        print(
+            "MAX OUTPUT :",
+            round(float(np.max(pred)), 4)
+        )
+
+        print(
+            "MIN OUTPUT :",
+            round(float(np.min(pred)), 4)
         )
 
         print(
